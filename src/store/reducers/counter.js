dd4c70ee5ef466a2ee9edce1ca8698a2e2ab9491
Nay Lin Aung <aunhtet@gmail.com>
2020-01-16 16:18:09 +0630

    First commit

---
+++ src/store/reducers/counter.js
@@ -0,0 +1,34 @@
+import * as actionType from '../actions/actions';
+
+const initialState = {
+    counter : 0
+}
+
+const reducer = (state = initialState, action) => {
+
+    switch(action.type){
+        case actionType.INCREMENT:
+            const newState = Object.assign({}, state);
+            newState.counter = state.counter + 1
+            return newState;
+        case actionType.DECREMENT:
+            return {
+                ...state,
+                counter: state.counter - 1
+            }
+        case actionType.ADD:
+            return {
+                ...state,
+                counter: state.counter + action.val
+            }
+        case actionType.SUBTRACT:
+            return{
+                ...state,
+                counter: state.counter - action.val
+            }
+        default:
+            return state;
+    }
+};
+
+export default reducer;
